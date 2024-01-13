2caef1413ec1e002a8d7c2d246c39809edf40b13
PrajjwalSingh24 <prajjwal.singh@delhivery.com>
2024-01-13 23:43:04 +0530

    tailwind-setup

---
+++ src/HeaderComponent/Header.tsx
@@ -0,0 +1,16 @@
+import './Header.css';
+import '../index.css';
+
+export default function Header(){
+    return(
+        <>
+        <div className='max-w-full max-h-10 bg-dark'>
+            <div></div>
+            <div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+        </>
+    )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      'dark': '#2a2c34',
+      'light': '#c8daea',
+      'blue': '#07adcb',
+      'green': '#238273'
+    }
+  },
+  plugins: [],
+}
+
