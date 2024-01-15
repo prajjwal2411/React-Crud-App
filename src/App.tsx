206ea9ce0432118e306d98bc8d3df38f218eb743
PrajjwalSingh24 <prajjwal.singh@delhivery.com>
2024-01-15 23:56:48 +0530

    header-design-complete

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,6 @@ function App() {
     <>
       <Header />
       
-      <h1 className="text-3xl font-bold">
-        Hello world!
-      </h1>
     </>  
   );
 }
--- src/HeaderComponent/Header.tsx
+++ src/HeaderComponent/Header.tsx
@@ -1,16 +1,18 @@
 import './Header.css';
 import '../index.css';
+import { User, Plus } from "@phosphor-icons/react";
+
 
 export default function Header(){
     return(
-        <>
-        <div className='max-w-full max-h-10 bg-dark'>
-            <div></div>
-            <div>
-                <div></div>
-                <div></div>
-            </div>
+    <>
+    <div className='flex flex-wrap content-center justify-between max-w-full h-14 bg-dark'>
+        <div className='text-blue text-2xl'>CRUD Tutorial</div>
+        <div className='text-blue flex options'>
+            <div><User size={32}/></div>
+            <div><Plus size={32}/></div>
         </div>
-        </>
+    </div>
+    </>
     )
 }
