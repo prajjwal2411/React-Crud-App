15138dd3bab39ae6c62d7063c21ed47641a22a82
PrajjwalSingh24 <prajjwal.singh@delhivery.com>
2024-01-19 23:18:26 +0530

    header-completed-with-design-lib

--- src/HeaderComponent/Header.tsx
+++ src/HeaderComponent/Header.tsx
@@ -1,18 +1,32 @@
 import './Header.css';
-import '../index.css';
-import { User, Plus } from "@phosphor-icons/react";
 
+import React, { useState } from 'react';
+import { MailOutlined } from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Menu } from 'antd';
 
-export default function Header(){
-    return(
-    <>
-    <div className='flex flex-wrap content-center justify-between max-w-full h-14 bg-dark'>
-        <div className='text-blue text-2xl'>CRUD Tutorial</div>
-        <div className='text-blue flex options'>
-            <div><User size={32}/></div>
-            <div><Plus size={32}/></div>
-        </div>
-    </div>
-    </>
-    )
-}
+const items: MenuProps['items'] = [
+  {
+    label: 'Employees',
+    key: 'list',
+    icon: <MailOutlined />,
+  },
+  {
+    label: 'Add Employee',
+    key: 'add',
+    icon: <MailOutlined />,
+  }
+];
+
+const App: React.FC = () => {
+  const [current, setCurrent] = useState('');
+
+  const onClick: MenuProps['onClick'] = (e) => {
+    console.log('click ', e);
+    setCurrent(e.key);
+  };
+
+  return <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />;
+};
+
+export default App;
